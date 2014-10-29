3c02148202e1f3653b2c90ac49071e03200772e9
silicarich <nicole.flokos@gmail.com>
2014-10-28 22:29:43 -0400

    functionified!
    though, it's printing out twice.

--- rainbowify.js
+++ rainbowify.js
@@ -1,20 +1,28 @@
-function () {
-var s = $('span.text').text();
-var rainbowified = '';
-var colors =
-	["red"
-	, "yellow"
-	, "orange"
-	, "green"
-	, "blue"
-	, "purple"
-	, "brown"
-	, "black"
-	];
-for (var i = 0, temp = 0; i < s.length; i++, temp++) {
-    if (temp >= colors.length)
-        temp = 0;
-    rainbowified += '<span class="' + colors[temp] + '">' + s[i] + '</span>';
-}
-$('span.text').html(rainbowified);
+
+$.fn.rainbowify = function () {
+
+    if (!$(this).attr('id'))
+        var textSelector = '.' + $(this).attr('class');
+    else
+        var textSelector = '.' + $(this).attr('id');
+
+    var s = $(textSelector).text();
+    var rainbowified = '';
+    var colors =
+        ["red"
+        , "yellow"
+        , "orange"
+        , "green"
+        , "blue"
+        , "purple"
+        , "brown"
+        , "black"
+        ];
+    for (var i = 0, temp = 0; i < s.length; i++, temp++) {
+        if (temp >= colors.length)
+            temp = 0;
+        rainbowified += '<span class="' + colors[temp] + '">' + s[i] + '</span>';
+    }
+    console.log(rainbowified);
+  //  $(textSelector).html(rainbowified);
 }
